0ad55bf43511feff66064961d6a4a808781e9d5d
LinYongW <linyongw@gmail.com>
2014-12-02 17:33:33 +0800

    add second

---
+++ EffectiveJava/src/main/java/cn/ly/java/EffectiveJava/second/NutritionFacts.java
@@ -0,0 +1,61 @@
+package cn.ly.java.EffectiveJava.second;
+
+/**
+ * 
+ *@explanation Builder Pattern
+ *@author YongLin
+ *@email  yonglin@hengtiansoft.com
+ *@time   2014年12月2日下午5:15:27
+ */
+public class NutritionFacts {
+
+	private final int servingSize;
+	private final int servings;
+	private final int calories;
+	private final int fat;
+	private final int sodium;
+	private final int carbohydrate;
+	
+	public static class Builder{
+		private final int servingSize;
+		private final int servings;
+		
+		private int calories = 0;
+		private int fat = 0;
+		private int  carbohydrate = 0;
+		private int sodium = 0;
+		
+		public Builder(int servingSize, int servings){
+			this.servingSize = servingSize;
+			this.servings = servings;
+		}
+		
+		public Builder calories(int val){
+			calories = val;
+			return this;
+		}
+		
+		public Builder carbohydrate(int val){
+			carbohydrate = val;
+			return this; 
+		}
+		
+		public Builder sodium(int val){
+			sodium = val;
+			return this;
+		}
+		
+		public NutritionFacts build(){
+			return new NutritionFacts(this);
+		}
+	}
+	
+	private NutritionFacts(Builder builder){
+		servingSize = builder.servingSize;
+		servings = builder.servings;
+		calories = builder.calories;
+		fat = builder.fat;
+		sodium = builder.sodium;
+		carbohydrate = builder.carbohydrate;
+	}
+}
